a0883b1263c77ccf984e819d3736016f44cc8088
python357-1 <jb2101554@gmail.com>
2020-11-26 11:23:29 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const multer = require('multer')
+const GridFsStorage = require('multer-gridfs-storage')
+const Grid = require('gridfs-stream')
+
+mongoose.connect('mongodb://localhost/musicdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+let gfs;
+mongoose.connection.once('open', () => {
+    gfs = Grid(mongoose.connection.db, mongoose.mongo);
+    gfs.collection('songs')
+})
+
+let storage = new GridFsStorage({
+    url: "mongodb://localhost/musicdb",
+    file: (req, file) => {
+        return new Promise(
+            (resolve, reject) => {
+                const fileInfo = {
+                    filename: file.originalname,
+                    bucketName: "songs"
+                }
+                resolve(fileInfo)
+            }
+        )
+    }
+})
+
+const upload = multer({ storage })
+
+const Schema = mongoose.Schema;
+
+const SongSchema = new Schema({
+    title: String,
+    artist: String,
+    album: String,
+    release: String,
+    source_file: String
+})
+
+const Song = mongoose.model('SongData', SongSchema)
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({data: {
+        version: "1.0.0",
+        release_date: "11/26/2020",
+        author: "Jordan Banta-Clark"
+    }})
+})
+
+app.get('/songs/', (req, res) => {
+    Song.find((err, songs) => {
+        res.json(songs)
+    })
+})
+
+app.post('/song', upload.single('source_file'), (req, res) => {
+    let song = new Song({
+        title: req.body.title.toLowerCase(),
+        artist: req.body.artist.toLowerCase(),
+        album: req.body.album.toLowerCase(),
+        release: req.body.release.toLowerCase(),
+        source_file: req.file.id,
+    })
+    song.save()
+    res.json({ success: "success"})
+})
+
+app.get('/song/:title', (req, res) => {
+    Song.findOne({ title: req.params.title}, (err, song) => {
+        if (song == null) {
+            res.json({err: "no song found"})
+        } else {
+            res.json(song)
+        }
+    })
+})
+
+app.get("/song/:title/source", (req, res) => {
+    Song.findOne({ title: req.params.title }, (err, song) => {
+        if (song == null) {
+            res.json({err: "no song found"})
+        } else {
+            let readstream = gfs.createReadStream({ _id: song.source_file });
+            readstream.pipe(res) 
+        }
+    })
+})
+
+app.listen(3000)
